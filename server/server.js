cdcd4ba141684a69458804b9ccfc803ec7b2f69c
muratyasin <mkubilay@hotmail.com>
2018-07-13 15:30:37 +0300

    Port fix

--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ const express=require('express');
 
 const publicPath=path.join(__dirname, '../public');
 
+const port = process.env.PORT || 3000;
 
 var app = express();
 
 app.use(express.static(publicPath));
 
 
-app.listen(3000, ()=>{
-  console.log(`Started up on 3000`);
+app.listen(port, ()=>{
+  console.log(`Started up on ${port}`);
 });
