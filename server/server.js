95dea380c02f1a1002ed384be76d15f4e2230f56
muratyasin <mkubilay@hotmail.com>
2018-07-13 15:20:45 +0300

    Init commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express=require('express');
+
+const publicPath=path.join(__dirname, '../public');
+
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+
+app.listen(3000, ()=>{
+  console.log(`Started up on 3000`);
+});
