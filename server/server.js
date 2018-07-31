7314d95045bd06841205005a218003739a07db85
muratyasin <mkubilay@hotmail.com>
2018-07-31 10:34:57 +0300

    Add form for messages

--- server/server.js
+++ server/server.js
@@ -23,34 +23,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New user connected');
 
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text:'What is going on',
-  //   createAt: 123
-  // });
-
-  // socket.emit('newMessage', {
-  //   from: 'mike@example.com',
-  //   text:'What is going on',
-  //   createAt: 123
-  // });
-
   socket.emit('newMessage', generateMessage( 'Admin','Welcome to the chat app'));
 
   socket.broadcast.emit('newMessage', generateMessage('Admin','A new user has joined'));
 
-  socket.on('createMessage', (message)=>{
+  socket.on('createMessage', (message,callback)=>{
     console.log('createMessage', message);
-    // io.emit('newMessage', { //It sends to message to every user
-    //   from: message.from,
-    //   text:message.text,
-    //   createdAt: new Date().getTime()
-    // });
-    socket.broadcast.emit('newMessage', generateMessage(message.from,message.text)) ;//It sends to message to every user
-    //   from: message.from,
-    //   text:message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    io.emit('newMessage', generateMessage(message.from,message.text)) ;//It sends to message to every user
+    callback('This is from the server');
   });
 
 
